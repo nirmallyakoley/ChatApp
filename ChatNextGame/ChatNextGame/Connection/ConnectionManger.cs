857175b3b1858a9977d986622f8695beb5155747
nirmallyakoley@gmail.com <nirmallyakoley@gmail.com>
2021-08-01 19:14:17 +0000

    First push

---
+++ ChatNextGame/ChatNextGame/Connection/ConnectionManger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatNextGame.Connection
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> _socketCollection = new ConcurrentDictionary<string, WebSocket>();
+
+        public WebSocket GetSocketById(string id)
+        {
+            return _socketCollection.FirstOrDefault(p => p.Key == id).Value;
+        }
+
+        public ConcurrentDictionary<string, WebSocket> GetAll()
+        {
+            return _socketCollection;
+        }
+
+        public string GetId(WebSocket socket)
+        {
+            return _socketCollection.FirstOrDefault(p => p.Value == socket).Key;
+        }
+        public async Task AddSocket(WebSocket socket)
+        {
+           await Task.Run(() =>_socketCollection.TryAdd(CreateConnectionId(), socket));
+        }
+
+        public async Task RemoveSocket(string id)
+        {
+            WebSocket socket;
+            _socketCollection.TryRemove(id, out socket);
+
+            await socket.CloseAsync(closeStatus: WebSocketCloseStatus.NormalClosure,
+                                    statusDescription: "Closed by the ConnectionManager",
+                                    cancellationToken: CancellationToken.None);
+        }
+
+        public int SocketCount()
+        {
+            return _socketCollection.Count;
+        }
+        private string CreateConnectionId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ ChatNextGame/ChatNextGame/Handler/WebSocketHandler.cs
@@ -0,0 +1,70 @@
+﻿using ChatNextGame.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatNextGame.Handler
+{
+    public class WebSocketHandler
+    {
+        protected ConnectionManager WebSocketConnectionManager { get; set; }
+
+        public WebSocketHandler(ConnectionManager webSocketConnectionManager)
+        {
+            WebSocketConnectionManager = webSocketConnectionManager;
+        }
+
+        public  async Task OnConnected(WebSocket socket)
+        {
+            if (WebSocketConnectionManager.SocketCount() <= 20)
+            {
+                await WebSocketConnectionManager.AddSocket(socket);
+                var socketId = WebSocketConnectionManager.GetId(socket);
+                await SendMessageToAllAsync($"{socketId} is now connected");
+            }
+            else 
+            {
+                await SendMessageToAllAsync($"Chat Room is now full");
+            }
+        }
+
+        public  async Task OnDisconnected(WebSocket socket)
+        {
+            await WebSocketConnectionManager.RemoveSocket(WebSocketConnectionManager.GetId(socket));
+        }
+
+        public async Task SendMessageAsync(WebSocket socket, string message)
+        {
+            if (socket.State != WebSocketState.Open)
+                return;
+
+            await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),offset: 0,count: message.Length),messageType: WebSocketMessageType.Text, endOfMessage: true,cancellationToken: CancellationToken.None);
+        }
+
+        public async Task SendMessageAsync(string socketId, string message)
+        {
+            await SendMessageAsync(WebSocketConnectionManager.GetSocketById(socketId), message);
+        }
+
+        public async Task SendMessageToAllAsync(string message)
+        {
+            foreach (var pair in WebSocketConnectionManager.GetAll())
+            {
+                if (pair.Value.State == WebSocketState.Open)
+                    await SendMessageAsync(pair.Value, message);
+            }
+        }
+
+       
+        public async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = WebSocketConnectionManager.GetId(socket);
+            var message = $"{socketId} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await SendMessageToAllAsync(message);
+        }
+    }
+}
